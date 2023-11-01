bf73b47d0493b87ec85feb41906624d57a1403fb
Praveen Kumar <31979732+praveenrudhraboina@users.noreply.github.com>
2023-10-31 19:33:05 -0700

    Update Code_smells_example.py
    fix 2

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,9 +1,17 @@
-#Code Smells Example for long method
 def calculate_total_price(items):
     total = 0
     for item in items:
-        if item.quantity > 0 and item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+        total += calculate_item_price(item)
     return total
+
+def calculate_item_price(item):
+    if item.quantity > 0:
+        return calculate_discounted_price(item.price)
+    else:
+        return 0
+
+def calculate_discounted_price(price):
+    if price > 100:
+        return price * 0.9
+    else:
+        return price * 0.95
